e8dec121a6d5d7758b3d7943246e065127e35296
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-03-10 17:33:30 +0200

    added validation and name delete function

--- App1/app1.js
+++ App1/app1.js
@@ -14,15 +14,53 @@ createApp({
 
         const data= reactive(defaultState);
 
+        const haveTwoNames= computed(()=>{
+            return data.names.length>1
+        })
+
+        const showResult=()=>{
+            data.state=false;
+        }
+
+        const validate=(inputValue)=>{
+            data.error="";
+
+            if(inputValue===""){
+                data.error="No empty name!";
+                return false;
+            }else if(inputValue.length<3){
+                data.error="Name can't be that short!";
+                return false;
+            }else if(inputValue.length>15){
+                data.error="Name can't be that long!";
+                return false;
+            }
+            if(data.names.includes(inputValue)){
+                data.error="Names must be unique!";
+                return false;
+            }
+
+            return true;
+        }
+
         const addNameToList= ()=>{
             const userName=data.inputName;
 
-            data.names.push(userName);
-            data.inputName="";
-
-           // console.log(data.names)
+            if(validate(userName)){
+                data.names.push(userName);
+                data.inputName="";
+                data.showError=false;
+                //console.log(data.names)
+            }else{
+                data.showError=true;
+            }
+            
         }
 
-        return{ data, addNameToList }
+        const removeName=(index)=>{
+            data.names.splice(index,1);
+        }
+
+        return{ data, addNameToList, removeName, haveTwoNames, showResult }
     }
 }).mount("#app")
