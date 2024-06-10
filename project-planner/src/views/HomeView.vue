f94b0099e7743b273b8802e033bf8065991dfb57
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-06-10 14:41:40 +0300

    added possibility to filter the project (completed/not)-finished

--- project-planner/src/views/HomeView.vue
+++ project-planner/src/views/HomeView.vue
@@ -1,8 +1,9 @@
 
 <template>
   <main class="home">
+    <FilterNav @filterChange="current=$event" :current="current"/>
       <div v-if="projects.length">
-          <div v-for="project in projects" :key="project.id">
+          <div v-for="project in filteredProjects" :key="project.id">
               <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
           </div>
       </div>
@@ -10,10 +11,12 @@
 </template>
 
 <script setup>
-  import { ref,onMounted } from 'vue';
-  import SingleProject from '../components/SingleProject.vue'
+  import { ref,onMounted,computed } from 'vue';
+  import SingleProject from '../components/SingleProject.vue';
+  import FilterNav from '../components/filterNav.vue';
   
   const projects=ref([]);
+  const current=ref('all')
 
   const handleDelete=(projectID)=>{
     projects.value=projects.value.filter((project)=>{
@@ -28,7 +31,17 @@
     p.complete=!p.complete
     
   }
+  
+  const filteredProjects=computed(()=>{
+      if(current.value === 'completed'){
+          return projects.value.filter(project=> project.complete);
+      }
+      if(current.value === 'ongoing'){
+          return projects.value.filter(project=> !project.complete);
+      }
 
+      return projects.value;
+  })
   onMounted(()=>{
     fetch('http://localhost:3000/projects')
     .then(response=>response.json())
