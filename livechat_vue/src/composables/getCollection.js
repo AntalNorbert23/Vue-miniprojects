598f22b73a0dee5c58c3c9f047a0b37399581984
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-06-18 15:37:45 +0300

    added get message collection from database composable

---
+++ livechat_vue/src/composables/getCollection.js
@@ -0,0 +1,33 @@
+import { ref } from "vue";
+import { DB } from "@/firebase/config";
+import { collection, query,orderBy,onSnapshot } from "firebase/firestore";
+
+
+const getCollection=(collectionName)=>{
+    const documents=ref(null);
+    const error=ref(null);
+
+    let colRef=collection(DB,collectionName);
+    const q=query(colRef,orderBy('createdAt'));
+    
+    onSnapshot(q,(snapshot)=>{
+        let results=[];
+        snapshot.docs.array.forEach(doc => {
+            const data=doc.data();
+
+            if(data.createdAt){
+                results.push({...data, id: doc.id})
+            }
+        });
+        documents.value=results;
+        error.value=null;
+    },(err)=>{
+        console.log(err.message);
+        documents.value=null;
+        error.value='Could not fetch data';
+    })
+
+    return{ documents,error }
+}
+
+export default getCollection
