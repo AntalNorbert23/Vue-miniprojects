8480a5fb0fbb83f833df463256fd7c0f6e9152ca
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-07-29 15:21:28 +0300

    added password visibility toggling

--- livechat_vue/src/components/LoginForm.vue
+++ livechat_vue/src/components/LoginForm.vue
@@ -1,8 +1,12 @@
 <template>
     <form @submit.prevent="handleSubmit">
         <input type="email" required placeholder="E-mail" v-model="email">
-        <input type="password" required placeholder="Password" v-model="password">
-        <div class="error">{{ error }}</div>
+        <div class="password-container">
+            <input :type="passwordInputType" required placeholder="Password" v-model="password">
+            <span class="fa fa-eye" @click="togglePasswordVisibility"></span>
+        </div>
+
+        <div class="error" v-if="error">{{ error.message }}</div>
         <button>
             Log in
         </button>
@@ -10,14 +14,24 @@
 </template>
 
 <script setup>
-    import { ref } from 'vue';
+    import { ref,computed } from 'vue';
     import useLogin from '@/composables/useLogin';
     
     const email=ref('');
     const password=ref('');
     const emit=defineEmits(['login']);
 
+    const passVisible=ref(false);
+
     const { error,login }=useLogin();
+
+    const passwordInputType=computed(()=>{
+        return passVisible.value ? 'text' : 'password';
+    })
+
+    const togglePasswordVisibility=()=>{
+        passVisible.value=!passVisible.value
+    }
 
     const handleSubmit= async ()=>{
         await login(email.value, password.value);
@@ -28,3 +42,17 @@
 
 
 </script>
+
+<style>
+    .password-container {
+        position: relative;
+    }
+    .password-container .fa-eye {
+    position: absolute;
+    right: 5px;
+    top: 50%;
+    transform: translateY(-50%);
+    cursor: pointer;
+    color: #888;
+}
+</style>
